18cb372adac24d978942d8a41bc17e14d0121c16
Matthew Nitzken <mjnitz02@gmail.com>
2018-01-21 20:53:35 -0500

    Added SurvivalShooter
    Added SurvivalShooter project, assets, first scene, environment, player, player scripts and animations.

---
+++ SurvivalShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6.0f;
+
+	private Vector3 movement;
+	private Animator anim;
+	private Rigidbody playerRigidbody;
+	private int floorMask;
+	private float camRayLength = 100.0f;
+
+	private void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	private void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal"); //Value of -1, 0, or 1. Snaps character to full speed vs GetAxis.
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+	}
+
+	//Movement, turning, and animation
+	private void Move(float h, float v)
+	{
+		movement.Set (h, 0.0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	private void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0.0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	private void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
+	}
+}
